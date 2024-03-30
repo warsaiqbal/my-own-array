ac2d41d66e5b5b7d1cae42a4805987f8677f0781
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-03-30 08:25:03 -0700

    Add files via upload

---
+++ 12. My own array/index.js
@@ -0,0 +1,5 @@
+var transport = ["Scooty", "Motorcycle", "Corolla", "Airplane"];
+console.log("I would like to buy a", transport[0]);
+console.log("I would like to own a Honda", transport[1]);
+console.log("I would like to drive a", transport[2]);
+console.log("I would like to travel in an", transport[3]);
---
+++ 12. My own array/index.ts
@@ -0,0 +1,7 @@
+let transport: string[] = ["Scooty", "Motorcycle", "Corolla", "Airplane"];
+
+console.log("I would like to buy a", transport[0]);
+console.log("I would like to own a Honda", transport[1]);
+console.log("I would like to drive a", transport[2]);
+console.log("I would like to travel in an", transport[3]);
+
